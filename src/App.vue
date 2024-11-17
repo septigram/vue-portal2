7ff2e91ee912dd5d10b6805ef7509272031f2a4f
septigram <wyvern@septigram.com>
2024-11-17 09:34:38 +0900

    tmp commit.

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ interface ComponentInfo {
   checked: boolean,
 };
 
-const version = 'v2.24.1114';
+const version = 'v2.24.1117';
 
 const showComponentEdit = ref(false);
 const defaultComponents: ComponentInfo[] = [
